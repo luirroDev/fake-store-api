9eab2eafe17f68200b03bb6f4e2ce4063e682ba2
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2023-07-18 12:26:45 -0400

    proyect init

---
+++ src/services/user.service.js
@@ -0,0 +1,33 @@
+const boom = require('@hapi/boom');
+const getConnection = require('../libs/postgres');
+
+class UserService {
+  constructor() {}
+
+  async create(data) {
+    return data;
+  }
+
+  async find() {
+    const client = await getConnection();
+    const response = await client.query('SELECT * FROM tasks');
+    return response.rows;
+  }
+
+  async findOne(id) {
+    return { id };
+  }
+
+  async update(id, changes) {
+    return {
+      id,
+      changes,
+    };
+  }
+
+  async delete(id) {
+    return { id };
+  }
+}
+
+module.exports = UserService;
