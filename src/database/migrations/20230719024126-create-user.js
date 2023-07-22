15d5a260904a7b75ac3b6c49364d59e41bfad03b
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2023-07-21 23:31:41 -0400

    managing migrations for production

--- src/database/migrations/20230719024126-create-user.js
+++ src/database/migrations/20230719024126-create-user.js
@@ -1,14 +1,32 @@
 'use strict';
 
 const { UserSchema, USERS_TABLE } = require('../models/user.model');
+const { CustomerSchema, CUSTOMER_TABLE } = require('../models/customer.model');
+const { ProductSchema, PRODUCT_TABLE } = require('../models/product.model');
+const { CategoySchema, CATEGORY_TABLE } = require('../models/category.model');
+const { OrdersSchema, ORDER_TABLE } = require('../models/order.model');
+const {
+  OrderProductSchema,
+  ORDER_PRODUCT_TABLE,
+} = require('../models/order-product.model');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable(USERS_TABLE, UserSchema);
+    await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
+    await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
+    await queryInterface.createTable(CATEGORY_TABLE, CategoySchema);
+    await queryInterface.createTable(ORDER_TABLE, OrdersSchema);
+    await queryInterface.createTable(ORDER_PRODUCT_TABLE, OrderProductSchema);
   },
 
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable(USERS_TABLE);
+    await queryInterface.dropTable(CUSTOMER_TABLE);
+    await queryInterface.dropTable(PRODUCT_TABLE);
+    await queryInterface.dropTable(CATEGORY_TABLE);
+    await queryInterface.dropTable(ORDER_TABLE);
+    await queryInterface.dropTable(ORDER_PRODUCT_TABLE);
   },
 };
